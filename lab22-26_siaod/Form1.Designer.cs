f36ae5c16b66caf22216b84f09b11f6f5ef139e0
AleksandrChernetsov <aleksandrchernetsov07@gmail.com>
2026-04-18 03:25:12 +0300

    Add SimpleTwoPhaseMergeSort algorithm (lab 22)

---
+++ lab22-26_siaod/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace lab22_26_siaod
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewCheckBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
+            Column6 = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            label1 = new Label();
+            button2 = new Button();
+            label2 = new Label();
+            numericUpDown2 = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, Column6 });
+            dataGridView1.Location = new Point(3, 1);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(618, 150);
+            dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = Color.Cyan;
+            dataGridViewCellStyle1.NullValue = false;
+            Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            Column1.HeaderText = "";
+            Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle2.BackColor = Color.Cyan;
+            Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            Column2.HeaderText = "";
+            Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Сравнения";
+            Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Присвоения";
+            Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            Column5.HeaderText = "Время";
+            Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            Column6.HeaderText = "Отсортировано?";
+            Column6.Name = "Column6";
+            Column6.Width = 115;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(3, 172);
+            button1.Name = "button1";
+            button1.Size = new Size(93, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Сортировать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(237, 173);
+            numericUpDown1.Maximum = new decimal(new int[] { 1569325056, 23283064, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 2;
+            numericUpDown1.Value = new decimal(new int[] { 1000000, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(132, 176);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Размер массива";
+            // 
+            // button2
+            // 
+            button2.BackgroundImageLayout = ImageLayout.None;
+            button2.Image = Properties.Resources.Gnome_application_exit;
+            button2.Location = new Point(562, 158);
+            button2.Name = "button2";
+            button2.Size = new Size(48, 44);
+            button2.TabIndex = 4;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(396, 176);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 6;
+            label2.Text = "% ОП";
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(439, 173);
+            numericUpDown2.Maximum = new decimal(new int[] { 1569325056, 23283064, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(62, 23);
+            numericUpDown2.TabIndex = 5;
+            numericUpDown2.Value = new decimal(new int[] { 10, 0, 0, 0 });
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            BackColor = SystemColors.GradientActiveCaption;
+            ClientSize = new Size(625, 213);
+            Controls.Add(label2);
+            Controls.Add(numericUpDown2);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Чернецов А.М. 24ВП1. Лаб 22-26";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+        private Button button2;
+        private Label label2;
+        private NumericUpDown numericUpDown2;
+        private DataGridViewCheckBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+    }
+}
---
+++ lab22-26_siaod/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System.Diagnostics;
+
+namespace lab22_26_siaod
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = 5;
+            dataGridView1.ColumnCount = 6;
+
+            dataGridView1.Rows[0].Cells[1].Value = "Простое 2ф";
+            dataGridView1.Rows[1].Cells[1].Value = "Простое 1ф";
+            dataGridView1.Rows[2].Cells[1].Value = "Естественное 2ф";
+            dataGridView1.Rows[3].Cells[1].Value = "Естественное 1ф";
+            dataGridView1.Rows[4].Cells[1].Value = "Поглощение";
+
+            dataGridView1.Rows[0].Cells[0].Value = true;
+            dataGridView1.Rows[1].Cells[0].Value = true;
+            dataGridView1.Rows[2].Cells[0].Value = true;
+            dataGridView1.Rows[3].Cells[0].Value = true;
+            dataGridView1.Rows[4].Cells[0].Value = true;
+
+            dataGridView1.RowHeadersVisible = false;
+        }
+
+        // Функция проверки упорядоченности массива 
+        private bool IsSorted(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                    return false;
+            }
+            return true;
+        }
+
+        // Алгоритм простого двухфазного слияния
+        private (long comparisons, long assignments, long time) SimpleTwoPhaseMergeSort(int[] a)
+        {
+            long comparisons = 0;
+            long assignments = 0;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int n = a.Length;
+            int[] b = new int[n];
+            int[] c = new int[n];
+            int seriesLength = 1;
+
+            while (seriesLength < n)
+            {
+                int bCount = 0;
+                int cCount = 0;
+                int i = 0;
+
+                while (i < n)
+                {
+                    for (int j = 0; j < seriesLength && i < n; j++)
+                    {
+                        b[bCount++] = a[i++];
+                        assignments++;
+                    }
+                    for (int j = 0; j < seriesLength && i < n; j++)
+                    {
+                        c[cCount++] = a[i++];
+                        assignments++;
+                    }
+                }
+
+                int aIdx = 0, bIdx = 0, cIdx = 0;
+
+                while (bIdx < bCount || cIdx < cCount)
+                {
+                    int bEnd = Math.Min(bIdx + seriesLength, bCount);
+                    int cEnd = Math.Min(cIdx + seriesLength, cCount);
+
+                    while (bIdx < bEnd && cIdx < cEnd)
+                    {
+                        comparisons++;
+                        if (b[bIdx] <= c[cIdx])
+                        {
+                            a[aIdx++] = b[bIdx++];
+                        }
+                        else
+                        {
+                            a[aIdx++] = c[cIdx++];
+                        }
+                        assignments++;
+                    }
+
+                    while (bIdx < bEnd)
+                    {
+                        a[aIdx++] = b[bIdx++];
+                        assignments++;
+                    }
+
+                    while (cIdx < cEnd)
+                    {
+                        a[aIdx++] = c[cIdx++];
+                        assignments++;
+                    }
+                }
+
+                seriesLength *= 2;
+            }
+
+            stopwatch.Stop();
+            return (comparisons, assignments, stopwatch.ElapsedMilliseconds);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Если галочка снята — стираем результаты 
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[0].Value == null || !(bool)dataGridView1.Rows[i].Cells[0].Value)
+                {
+                    dataGridView1.Rows[i].Cells[2].Value = null;
+                    dataGridView1.Rows[i].Cells[3].Value = null;
+                    dataGridView1.Rows[i].Cells[4].Value = null;
+                    dataGridView1.Rows[i].Cells[5].Value = null;
+                }
+            }
+
+            int size = (int)numericUpDown1.Value;
+            int[] originalArray = new int[size];
+
+            for (int i = 0; i < size; i++)
+                originalArray[i] = random.Next(size);
+
+            if ((bool)dataGridView1.Rows[0].Cells[0].Value)
+            {
+                int[] sortingArray = (int[])originalArray.Clone();
+                var result = SimpleTwoPhaseMergeSort(sortingArray);
+
+                dataGridView1.Rows[0].Cells[2].Value = result.comparisons;
+                dataGridView1.Rows[0].Cells[3].Value = result.assignments;
+                dataGridView1.Rows[0].Cells[4].Value = result.time + " мс";
+                dataGridView1.Rows[0].Cells[5].Value = IsSorted(sortingArray) ? "Да" : "Нет";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
